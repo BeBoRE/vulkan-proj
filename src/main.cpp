9b707b8bfa30a66f4d64cef0f915fe49b57fdf3a
Ayrton-Taede Tromp <ayrton@bebore.com>
2024-11-24 16:26:08 +0100

    Got debugging working

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,26 @@
-#include "A.h"
+#include <iostream>
+#include <dlfcn.h>
 
 int main()
 {
-  auto a = A();
+  #if defined _WIN32
+  HMODULE vuklan_library = LoadLibrary("vulkan-1.dll");
+  #elif defined __linux
+  void* vulkan_library = dlopen("libvulkan.so.1", RTLD_NOW);
+  #endif
 
-  a.print();
+  if (vulkan_library == nullptr) {
+    std::cout << "Could not connect with a Vulkan Runtime library.\n";
+    return false;
+  }
+
+  #ifdef NDEBUG
+    printf("Release configuration!\n");
+    #else
+    printf("Debug configuration!\n");
+    #endif
+
+  std::cout << "bladibla";
+
+  return true;
 }
